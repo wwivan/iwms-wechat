16f8072ac0f54ba36bf8e6c9b5326d855ac6f24c
wwivan <1024528728@qq.com>
2019-07-25 07:46:15 +0800

    aaaa

---
+++ src/component/Tarbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="m-tabbar">
+    <a class="m-tabbar-item is-active">
+      <span class="m-tabbar-item-icon">
+        <img src="../assets/images/ic_tab_home_normal.png" alt="" />
+      </span>
+      <span class="m-tabbar-item-text">
+        首页
+      </span>
+    </a>
+    <a class="m-tabbar-item">
+      <span class="m-tabbar-item-icon">
+        <img src="../assets/images/ic_tab_subject_normal.png" alt="" />
+      </span>
+      <span class="m-tabbar-item-text">
+        书影音
+      </span>
+    </a>
+    <a class="m-tabbar-item">
+      <span class="m-tabbar-item-icon">
+        <img src="../assets/images/ic_tab_status_normal.png" alt="" />
+      </span>
+      <span class="m-tabbar-item-text">
+        广播
+      </span>
+    </a>
+    <a class="m-tabbar-item">
+      <span class="m-tabbar-item-icon">
+        <img src="../assets/images/ic_tab_group_normal.png" alt="" />
+      </span>
+      <span class="m-tabbar-item-text">
+        小组
+      </span>
+    </a>
+    <a class="m-tabbar-item">
+      <span class="m-tabbar-item-icon">
+        <img src="../assets/images/ic_tab_profile_normal.png" alt="" />
+      </span>
+      <span class="m-tabbar-item-text">
+        我的
+      </span>
+    </a>
+  </div>
+</template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,46 +1,72 @@
 <template>
   <div class="h-100">
-    <div >
+    <div>
+      <div v-show="this.isshow == 1">
+        <router-link tag="div" to="/stockIn">入库登记</router-link>
+        <router-link tag="div" to="/stockOut">出库登记</router-link>
+        <router-link tag="div" to="/item/search">物料查询</router-link>
+        <router-link tag="div" to="/check/account">对账</router-link>
+      </div>
+      <div v-show="this.isshow == 2">
+        <router-link tag="div" to="/echart">智能图标</router-link>
+      </div>
+      <div v-show="this.isshow == 3">
+        <router-link tag="div" to="/account/security">账号与安全</router-link>
+        <router-link tag="div" to="/logOut">退出登录</router-link>
+      </div>
       <router-view></router-view>
     </div>
-
-    <div class="tab-bar d-flex jc-around b-0">
+    <div class="tab-bar d-flex jc-around" style="height:57px">
       <div>
-        
-        <span  @click="()=>{(this.isshow==1)?(this.isshow=0):(this.isshow=1)}" :class="(isshow==1)?'text-blue-dark':''">功能</span>
-        <div v-show="this.isshow==1">
-          <router-link tag="div" to="/stockIn">入库登记</router-link>
-          <router-link tag="div" to="/stockOut">出库登记</router-link>
-          <router-link tag="div" to="/item/search">物料查询</router-link>
-          <router-link tag="div" to="/check/account">对账</router-link>
-        </div>
+        <span
+          class="posi-f b-0"
+          style="height:57px;line-height:57px"
+          @click="
+            () => {
+              this.isshow == 1 ? (this.isshow = 0) : (this.isshow = 1);
+            }
+          "
+          :class="isshow == 1 ? 'text-blue-dark' : ''"
+          >功能</span
+        >
       </div>
       <div>
-        <span  @click="()=>{(this.isshow==2)?(this.isshow=0):(this.isshow=2)}" :class="(isshow==2)?'text-blue-dark':''">报表</span>
-        <div v-show="this.isshow==2">
-          <router-link tag="div" to="/echart">智能图标</router-link>
-        </div>
+        <span
+          class="posi-f b-0"
+          style="height:57px;line-height:57px"
+          @click="
+            () => {
+              this.isshow == 2 ? (this.isshow = 0) : (this.isshow = 2);
+            }
+          "
+          :class="isshow == 2 ? 'text-blue-dark' : ''"
+          >报表</span
+        >
       </div>
       <div>
-        <span @click="()=>{(this.isshow==3)?(this.isshow=0):(this.isshow=3)}" :class="(isshow==3)?'text-blue-dark':''">我的</span>
-        <div v-show="this.isshow==3">
-          <router-link tag="div" to="/account/security">账号与安全</router-link>
-          <router-link tag="div" to="/logOut">退出登录</router-link>
-        </div>
+        <span
+          class="posi-f b-0"
+          style="height:57px;line-height:57px"
+          @click="
+            () => {
+              this.isshow == 3 ? (this.isshow = 0) : (this.isshow = 3);
+            }
+          "
+          :class="isshow == 3 ? 'text-blue-dark' : ''"
+          >我的</span
+        >
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data(){
-    return{
-      isshow:0
-    }
+  data() {
+    return {
+      isshow: 0
+    };
   }
-}
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
-
+<style lang="scss" scoped></style>
